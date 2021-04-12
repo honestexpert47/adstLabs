72baa5e057f0c2fb3e0f0d3bad044bb6cbbd3576
honestexpert47 <dmytro.prokopiev@nure.ua>
2021-04-12 14:11:36 +0300

    Added property IsEmpty for class List

--- Lab 1/Lab 1/List.cs
+++ Lab 1/Lab 1/List.cs
@@ -7,5 +7,7 @@ namespace Lab_1
         public int Length { get; private set; }
         
         public Node<T> First { get; private set; }
+        
+        public bool IsEmpty => Length == 0;
     }
 }
