f8b2414bbd813d4f44106eb7b2c2aaf4ce2f14c3
honestexpert47 <dmytro.prokopiev@nure.ua>
2021-04-12 14:10:12 +0300

    Added property First for class List

--- Lab 1/Lab 1/List.cs
+++ Lab 1/Lab 1/List.cs
@@ -5,5 +5,7 @@ namespace Lab_1
     public class List<T> where T : IComparable
     {
         public int Length { get; private set; }
+        
+        public Node<T> First { get; private set; }
     }
 }
